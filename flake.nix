6090203fb3c88f0c3f3c0e6fb08208bf4c7c51c6
weqeqq <ggmodwear@gmail.com>
2024-09-28 16:27:31 +0300

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Description";
+
+  inputs = {
+    nixpkgs = {
+      url = "git+ssh://git@github.com/nixpkgs/nixos-unstable";
+    };
+  };
+
+  outputs = { nixpkgs, ... }: let 
+    cartographcf = nixpkgs.pkgs.stdenv.mkDerivation {
+      pname   = "CartographCF";
+      version = "1.0";
+
+      src = ./source;
+
+      installPhase = ''
+        install -Dm444 ./source/* -t $out/share/fonts/
+      '';
+    };
+  in {
+    packages.cartographcf = cartographcf;
+  };
+}
